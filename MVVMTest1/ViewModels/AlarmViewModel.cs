5515e59d80c439c6ce9530ca7436605ec1cbf68b
Raj Rao <raj.rao@plantweave.com.au>
2020-07-18 11:55:02 +1000

    Add project files.

---
+++ MVVMTest1/ViewModels/AlarmViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMTest1.ViewModels
+{
+    public class AlarmViewModel : BaseViewModel
+    {
+    }
+}
---
+++ MVVMTest1/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using MVVMTest1.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMTest1.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel selectedViewModel = new HomeViewModel();
+
+        public BaseViewModel SelectedViewModel { 
+            get => selectedViewModel; 
+            set
+            { 
+                selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            } 
+        }
+
+        public ICommand UpdateViewCommand { get; set; }
+
+        public MainViewModel()
+        {
+            UpdateViewCommand = new UpdateViewCommand(this);
+        }
+    }
+}
